904e254f9b79787ca97571106c7e7b68f13006f5
AntonioMMartel <60984007+AntonioMMartel@users.noreply.github.com>
2022-03-25 20:31:12 +0000

    Otro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,35 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ejercicio con Texto';
-  text1="1";
-  text2="2";
-  num1=0;
-  num2=0;
-  result="X";
+  title = 'Calculadora crema klk';
+  calc = '0'
+  eval = '0'
 
-  concatenate():void{
-    this.result=this.text1+this.text2;
+  clear():void {
+    this.calc = '0'
+    this.eval = '0'
   }
-  upper():void{
-    this.result=this.text1.toUpperCase();
+
+  pressKey(key: string): void {
   }
-  lower():void{
-    this.result=this.text1.toLowerCase();
+
+  pressSign(key: string): void {
   }
-  find():void{
-    this.result=(this.text1.indexOf(this.text2)).toString();
-  }
-  slice():void{
-    this.result=this.text1.slice(this.num1,this.num2);
-  }
-  length():void{
-    this.result=(this.text1.length).toString();
-  }
-  include():void{
-    this.result=String(this.text1.includes(this.text2));
-  }
-  split():void{
-    this.result=((this.text1).split('this.text2')).toString();
+  
+  evaluate(): void {
+    
   }
 }
